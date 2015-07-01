bfabeb589b18a67bed8b932b97ab884ab50ffeb1
Janne Pakarinen <janne.pakarinen@helsinki.fi>
2015-07-01 13:32:34 +0300

    sum4 added

---
+++ sum4/src/Sum.java
@@ -0,0 +1,13 @@
+public class Sum {
+    public static long factorial(long n) {
+        // BEGIN SOLUTION
+        long result = 1;
+        for (long i = 2; i <= n; ++i) {
+            result *= i;
+        }
+        return result;
+        // END SOLUTION
+        // STUB: return 0; // Please write your code here
+    }
+}
+
